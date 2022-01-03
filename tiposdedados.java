627c2f03c832ed41c1e4de4c971740a63e5e1fc0
mayara bueno <luziamayara83@gmail.com>
2022-01-03 15:41:02 -0300

    exercicio tipo de cadastro

---
+++ tiposdedados.java
@@ -0,0 +1,20 @@
+package Turma29;
+
+public class tiposdedados {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int idade = 10;
+		double preco =10;
+		char genero=  'm';
+		boolean maioridade =false;
+		
+		System.out.println("idade");
+		System.out.println("preco");
+		System.out.println("genero");
+		System.out.println("maioridade");
+
+	}
+
+}
