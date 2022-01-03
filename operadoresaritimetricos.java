3e754372cfca101b5fd00195efe5cb34eb41310b
mayara bueno <luziamayara83@gmail.com>
2022-01-03 15:53:42 -0300

    operadores aritimetrico exercicio para entendimento

---
+++ operadoresaritimetricos.java
@@ -0,0 +1,47 @@
+package Turma29;
+
+public class operadoresaritimetricos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int x = 9;
+		int y = 2;
+		
+		
+		int soma = x +y ;
+		int subtraçao = x -y;
+		int multiplicacao = x * y;
+		int divisao = x / y;
+		int resto = x % y ;
+		
+		System.out.println("resultado da adição" + soma );
+		System.out.println("resultado da adição" + subtraçao );
+		System.out.println("resultado da adição" +  multiplicacao);
+		System.out.println("resultado da adição" + divisao );
+		System.out.println("resultado da adição" + resto );
+		
+		
+		x++;
+		System.out.println("resultado do incremento x" + x);
+		 
+		y--;
+		System.out.println("resultado do incremento y" + y);
+		
+		x +=10;
+		System.out.println("resultado do incremento x" + x);
+		
+		y-= 10;
+		System.out.println("resultado do incremento y + y");
+
+
+
+		
+
+				
+				
+
+	}
+
+}
