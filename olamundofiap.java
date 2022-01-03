5e06a25c10abf49e765f7b08a11f06b881a0572f
mayara bueno <luziamayara83@gmail.com>
2022-01-03 15:27:27 -0300

    primeiro olá mundo fiap

---
+++ olamundofiap.java
@@ -0,0 +1,12 @@
+package Turma29;
+
+public class olamundofiap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("olá mundo fiap");
+
+	}
+
+}
