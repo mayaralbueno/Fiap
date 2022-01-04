92d1a22119a9097e26db2ad0e8543b61e7b7ea17
mayara bueno <luziamayara83@gmail.com>
2022-01-04 13:42:47 -0300

    exercicio para o entendimento de operadores

---
+++ OPERADOR.java
@@ -0,0 +1,36 @@
+package Turma29;
+
+public class OPERADOR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 5;
+		int y = 10;
+		int z = 15;
+		
+		
+		int result = x + y;
+	  System.out.println(result);
+
+	  
+	  result = x - y;
+	  System.out.println(result);
+	  
+	 result = x * y;
+	  System.out.println(result);
+	  
+	  
+	  result = x / y;
+	  System.out.println(result);
+	  
+	  
+	  result = x %y;
+	  System.out.println(result);
+	  
+	  
+	  
+	  
+	}
+
+}
