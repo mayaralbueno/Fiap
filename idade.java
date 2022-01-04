afa77833e02dccd32315f7690bdc293827f1f61d
mayara bueno <luziamayara83@gmail.com>
2022-01-04 14:52:58 -0300

    novo exercicio idade

---
+++ idade.java
@@ -0,0 +1,23 @@
+package Turma29;
+
+import java.util.Scanner;
+
+public class idade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+
+		int idade = 18;
+
+		if(idade >=18 && idade <70) {
+			System.out.println("maior de idade");
+		}else if(idade >=70){
+			System.out.println("terceira idade");
+		}if( idade >=100) {
+			System.out.println("centenaria");
+		}
+	}
+
+}
