38a4d0841945dc75eb872fa8d33e325bf4832a3a
mayara bueno <luziamayara83@gmail.com>
2022-01-04 15:01:40 -0300

    novo exercicio idade atualização

--- idade.java
+++ idade.java
@@ -7,9 +7,13 @@ public class idade {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int idade = 0;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("digite sua idade");
+		idade = sc.nextInt();
 
 
-		int idade = 18;
 
 		if(idade >=18 && idade <70) {
 			System.out.println("maior de idade");
