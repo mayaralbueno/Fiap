e3840fecea9f999f1bd55e3547536b1ee7926e73
mayara bueno <luziamayara83@gmail.com>
2022-01-04 14:18:10 -0300

    novo exercicio

---
+++ operadoresLogicos.java
@@ -0,0 +1,18 @@
+package Turma29;
+
+public class operadoresLogicos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int idade = 75;
+		
+		boolean precisavotar = idade >= 18 && idade<70;
+		
+		System.out.println("precisavotar");
+		
+
+	}
+
+}
