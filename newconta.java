cc75603e700ab0ef2826481c42c25fdf3d1ef45c
mayara bueno <luziamayara83@gmail.com>
2022-01-05 20:22:57 -0300

    atualização

--- newconta.java
+++ newconta.java
@@ -3,25 +3,36 @@ package Turma29;
 public class newconta {
 
 
-     private int numero;
-     private double saldo;
-     
-     
-     private double recuperarSaldo () {
-    	 return saldo;
-     }
-     public void depositar(double valor) {
-    	 saldo = saldo + valor;
-    	 
-     }
-     
-     public void setNumero(int numero) {
-    	 this.numero = numero;
-     }
-      public int getnumero () {
-    	  
-    	  return numero;
-      }
+	private int numero;
+	private double saldo;
+	
+
+	public newconta () {
+
+	}
+
+
+	public newconta(int numero, double saldo) {
+		this.numero = numero;
+		this.saldo = saldo;
+		
+	}
+
+	private double recuperarSaldo () {
+		return saldo;
+	}
+	public void depositar(double valor) {
+		saldo = saldo + valor;
+
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public int getnumero () {
+
+		return numero;
+	}
 
 
 
