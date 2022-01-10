1070f6153c3379f90feb7d6801f141d34942f6e9
mayara bueno <luziamayara83@gmail.com>
2022-01-10 18:45:05 -0300

    primeiro exercicio herança

---
+++ newanimal.java
@@ -0,0 +1,23 @@
+package Turma29;
+
+public class newanimal {
+	
+	private String locomove;
+	private String alimenta;
+	public String getLocomove() {
+		return locomove;
+	}
+	public void setLocomove(String locomove) {
+		this.locomove = locomove;
+	}
+	public String getAlimenta() {
+		return alimenta;
+	}
+	public void setAlimenta(String alimenta) {
+		this.alimenta = alimenta;
+	}
+	
+	
+	
+
+}
