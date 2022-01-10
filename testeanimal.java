e0d514183afaf48f2b687e132fd2d02c9141c0d2
mayara bueno <luziamayara83@gmail.com>
2022-01-10 19:08:38 -0300

    teste de exercicio animal

---
+++ testeanimal.java
@@ -0,0 +1,21 @@
+package Turma29;
+
+public class testeanimal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       newanimal newanimal = new newanimal();
+       newanimal.setAlimenta("todo animal se alimenta");
+       newanimal.setLocomove("todo animal se locomove");
+       
+       dog dog = new dog();
+       dog.setLatido("auauauau");
+       dog.setAlimenta("cachorro gosta de ração");
+       dog.setLocomove("cachorro se locomove com as 4 patas");
+       
+       
+       
+       
+	}
+
+}
