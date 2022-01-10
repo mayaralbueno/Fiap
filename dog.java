34ccc29ea857d40ff9f988065c45aa19dc50a458
mayara bueno <luziamayara83@gmail.com>
2022-01-10 19:01:01 -0300

    primeiro exercicio herança cachorro

---
+++ dog.java
@@ -0,0 +1,18 @@
+package Turma29;
+
+public class dog extends newanimal{
+	
+	private String latido;
+
+	public String getLatido() {
+		return latido;
+	}
+
+	public void setLatido(String latido) {
+		this.latido = latido;
+	}
+	
+	
+	
+
+}
