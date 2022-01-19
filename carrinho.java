8937b0745be704350a5738a3572f06a12e79c2d1
mayara bueno <luziamayara83@gmail.com>
2022-01-19 18:24:40 -0300

    atualização exercicio

---
+++ carrinho.java
@@ -0,0 +1,21 @@
+package Turma29;
+
+import java.util.Scanner;
+
+public class carrinho {
+	
+	Scanner sc = new Scanner(System.in);
+
+	// Lê os dois números
+	int numero1 = sc.nextInt();
+	int numero2 = sc.nextInt();
+
+	try {
+	  // Realiza a divisão
+	  int divisao = numero1 / numero2;
+	  // Exibe o resultado
+	  System.out.println("O resultado é: " + divisao);
+	} catch (ArithmeticException e) {
+	  System.err.println("Erro ao dividir!");
+	}
+	sc.close();
