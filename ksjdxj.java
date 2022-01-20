b52dd0509998d25ba90d532f4bd40147d5da0ef2
mayara bueno <luziamayara83@gmail.com>
2022-01-20 15:08:21 -0300

    atualização exercicio

---
+++ ksjdxj.java
@@ -0,0 +1,22 @@
+package Turma29;
+
+public class ksjdxj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+
+	        int i = 10;
+
+	       double d = i;
+
+	       float f = d;
+
+	        long l = i;
+
+
+
+	}
+
+}
