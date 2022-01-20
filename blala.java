2f92816c8223dc5a1d0258a44d4ed71d34b06a7e
mayara bueno <luziamayara83@gmail.com>
2022-01-20 15:08:48 -0300

    atualização exercicio

---
+++ blala.java
@@ -0,0 +1,24 @@
+package Turma29;
+
+public class blala {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		  int i = 10;
+
+	         int j = 20;
+
+	         i++;
+
+	         int r = i + j%2;
+
+	         r *= j;
+
+	         System.out.println(r);
+
+	    
+	}
+
+}
